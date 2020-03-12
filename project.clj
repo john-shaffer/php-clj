22132b491b5c0efbfc01adf64663e64b45b033b4
john-shaffer <jshaffer2112@gmail.com>
2020-03-12 11:41:19 -0500

    Update dependencies to avoid "clojure.lang.ExceptionInfo: Conflicting data-reader mapping" that occurs when another project includes org.flatland/ordered. I updated the ordered dependency to use the new name of org.flatland/ordered instead and updated the versions of all other dependencies to get the tests to run.

--- project.clj
+++ project.clj
@@ -1,14 +1,11 @@
-(defproject php-clj "0.4.1"
+(defproject org.clojars.john-shaffer/php-clj "0.5.0-SNAPSHOT"
   :description "Deserialize PHP into Clojure data structures and back again."
-  :url "https://github.com/mudge/php-clj"
-  :dependencies [[ordered "1.3.2"]]
+  :url "https://github.com/john-shaffer/php-clj"
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.flatland/ordered "1.5.7"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]
-                   :plugins [[lein-midje "3.0.0"]
-                             [lein-kibit "0.0.8"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5.0 {:dependencies [[org.clojure/clojure "1.5.0"]]}
-             :1.5.1 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
-  :aliases {"all" ["with-profile" "dev,1.4:dev,1.5.0:dev,1.5.1"]})
+  :profiles {:dev {:dependencies [[midje "1.9.9"]]
+                   :plugins [[lein-midje "3.2.2"]
+                             [lein-kibit "0.1.8"]]}})
 
