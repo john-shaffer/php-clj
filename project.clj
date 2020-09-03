462809cb504dea400e8a0308cbc4956d66c7aca8
John Shaffer <jshaffer2112@protonmail.com>
2020-09-03 16:45:35 -0500

    Use ordered 1.5.9.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Deserialize PHP into Clojure data structures and back again."
   :url "https://github.com/john-shaffer/php-clj"
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.flatland/ordered "1.5.7"]]
+                 [org.flatland/ordered "1.5.9"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :profiles {:dev {:dependencies [[midje "1.9.9"]]
