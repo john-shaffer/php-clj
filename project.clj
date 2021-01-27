29f45a714289b673b6d1c88fbfd9fd30fd9cbc6f
John Shaffer <jshaffer2112@protonmail.com>
2021-01-26 23:25:55 -0600

    v1.0.0-alpha

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.john-shaffer/php-clj "0.5.1"
+(defproject org.clojars.john-shaffer/php-clj "1.0.0-alpha"
   :description "Deserialize PHP into Clojure data structures and back again."
   :url "https://github.com/john-shaffer/php-clj"
   :dependencies [[org.clojure/clojure "1.10.1"]
