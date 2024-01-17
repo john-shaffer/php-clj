d896191c275b9528c250e8f5e7cccf6cbddde4b7
John Shaffer <jdsha@pm.me>
2024-01-16 23:41:34 -0600

    Add nix devShell

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "php-clj";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      with import nixpkgs { inherit system; }; {
+        devShells.default = mkShell {
+          buildInputs = [
+            jdk
+            leiningen
+          ];
+        };
+      });
+}
