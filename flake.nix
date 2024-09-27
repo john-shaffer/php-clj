b2b887b4f5d09acc8817c0a49c80d07a3f65fd5d
John Shaffer <jdsha@pm.me>
2024-09-27 10:53:59 -0500

    Upgrade deps

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "php-clj";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
--- project.clj
+++ project.clj
@@ -2,10 +2,10 @@
   :description "Deserialize PHP into Clojure data structures and back again."
   :url "https://github.com/john-shaffer/php-clj"
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.flatland/ordered "1.15.11"]]
+                 [org.flatland/ordered "1.15.12"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:dependencies [[midje "1.9.9"]]
+  :profiles {:dev {:dependencies [[midje "1.10.10"]]
                    :plugins [[lein-midje "3.2.2"]
-                             [lein-kibit "0.1.8"]]}})
+                             [lein-kibit "0.1.11"]]}})
 
