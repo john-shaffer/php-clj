7e7e3a54b3420473295133cfbaaf9b2f1d3e8a42
John Shaffer <jdsha@pm.me>
2025-08-04 10:43:08 -0500

    Upgrade dev deps

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "php-clj";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
